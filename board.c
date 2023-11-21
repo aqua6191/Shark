7a1d2f0330f905c5c960cc9dc28b0d4eeed34ae8
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-11-21 16:57:00 +0900

    01-03(수정)

--- board.c
+++ board.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "board.h"
 
 #define N_BOARD          15
-#define BOARDSTATUS_OK    1
-#define BOARDSTATUS_NOK   0
+#define N_COINPOS          12
+#define MAX_COIN         4
 
 static int board_status[N_BOARD];
-
 static int board_coin[N_BOARD];
 
 static int board_sharkPosition;
 
-int board_initboard(void);
+int board_initboard(void){
+    int i;
+    for(i=0;i<N_BOARD;i++){
+                           board_status[i] = BOARDSTATUS_OK;
+                           board_coin[i] = 0;
+                           }
+    for(i=0;i<N_COINPOS;i++){
+                             int flag = 0;
+                             while(flag == 0){
+                                     int allocPos = rand()%N_BOARD;
+                                     if (board_coin[allocPos] == 0){
+                                                              board_coin[allocPos] = rand()%MAX_COIN +1;
+                                                              flag = 1; //무한루프 돌아서 빠져나오려고 사용... 
+                                                              }
+                                     }
+                             }
+                             return 0;    
+    }
 int board_printBoardStatus(void){
     int i;
     
@@ -25,6 +42,7 @@ int board_printBoardStatus(void){
                            }
                            printf("|\n");
                            printf("--------------------------------\n");
+                           return 0;
     }
 
 int board_getBoardStatus(int pos){
@@ -32,14 +50,13 @@ int board_getBoardStatus(int pos){
     }
     
     int board_getBoardCoin(int pos){
-     
     int coin = board_coin[pos];
     board_coin[pos] = 0;
     return coin;
     }
     
     
-int board_getSharkPosition(void);
-int board_stepShark(void);
+//int board_getSharkPosition(void);
+//int board_stepShark(void);
 
 
